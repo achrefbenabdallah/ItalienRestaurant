1d21c036f2e1f0dbe1c9129ae048ed9dd3863eee
maher ben nacef <35740936+MaherBenNacef@users.noreply.github.com>
2020-12-14 22:23:33 +0100

    DataBase+DTO

--- src/main/java/com/example/demo/models/Client.java
+++ src/main/java/com/example/demo/models/Client.java
@@ -1,13 +1,15 @@
-package com.example.models;
+package com.example.demo.models;
 
 
 import java.time.LocalDate;
+import java.util.List;
 
-
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -25,4 +27,6 @@ public class Client {
 	private LocalDate dateNaissance;
 	private String courriel;
 	private String tel;
+	@OneToMany(mappedBy = "client",cascade = CascadeType.REMOVE)
+	private List<Ticket> tickets;
 }
